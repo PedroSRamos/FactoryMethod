0904654ece03b0eedf6763ed316b95371515c1b5
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2021-10-15 22:09:01 -0300

    Delete college-java-factory-main/java/src/br/edu/univas directory

--- college-java-factory-main/java/src/br/edu/univas/vo/Celular.java
+++
@@ -1,7 +0,0 @@
-package br.edu.univas.vo;
-
-public class Celular {
-    public void turnOn(){
-        System.out.println("Ligando celular");
-    }
-}
--- college-java-factory-main/java/src/br/edu/univas/vo/Tv.java
+++
@@ -1,7 +0,0 @@
-package br.edu.univas.vo;
-
-public class Tv {
-    public void turnOn(){
-        System.out.println("Ligando tv");
-    }
-}
